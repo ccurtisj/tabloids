daadf1fef3ab8d405cf32e031ef51659d13c96e0
Christopher Curtis <ccurtisj@gmail.com>
2012-10-10 09:48:07 -0500

    Need to remove the isolated namespace to have access to routers in host app

--- lib/tabloids/engine.rb
+++ lib/tabloids/engine.rb
@@ -1,8 +1,6 @@
 module Tabloids
   class Engine < ::Rails::Engine
     
-    isolate_namespace Tabloids
-    
     initializer "static assets" do |app|
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
