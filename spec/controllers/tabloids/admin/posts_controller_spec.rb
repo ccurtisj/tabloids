6911f076f160667901669f96156cca27270f81e6
Christopher Curtis <ccurtisj@gmail.com>
2012-10-10 09:49:22 -0500

    Forgot to commit the specs for creation and deletion

---
+++ spec/controllers/tabloids/admin/posts_controller_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+module Tabloids
+  describe Admin::PostsController do
+  
+    describe "#create" do
+      
+      context "when it is successful" do
+        
+        before do
+          post :create, :post => {:title => 'foo', :content => 'bar'}
+        end
+        
+        it "creates a post" do
+          post = Post.last
+          post.title.should eq 'foo'
+          post.content.should eq 'bar'
+        end
+        
+        it "redirects to the #index page" do
+          response.should redirect_to admin_posts_path
+        end
+      end
+      
+      context "when it is unsuccessful" do
+        
+        before do
+          post :create, :post => {:title => '', :content => ''}
+        end
+        
+        it "does not create a new post" do
+          Post.count.should eq 0
+        end
+        
+        it "renders the new template" do
+          response.should render_template('new')
+        end
+      end
+    end
+    
+    describe "#destroy" do
+    
+      it "destroys a given post" do
+        post = Tabloids::Post.create!(:title => 'lorem', :content => "ipsum")
+        delete :destroy, :id => post.id
+        Post.where(:id => post.id).should have(0).posts
+      end
+    end
+  end
+end
